d571546b45106a745e421d21d3814d9b742a8fd9
NoBodyKnowsTheTroubleIHaveSeen <whhwkm100@163.com>
2017-02-01 14:52:56 +0800

    tuling

---
+++ src/main/java/org/whh/robot/TulingConstant.java
@@ -0,0 +1,12 @@
+package org.whh.robot;
+
+public class TulingConstant {
+	public static final int CODE_TEXT = 100000;// 文本类
+	
+	public static final int CODE_LINK = 200000;// 链接类
+	
+	public static final int CODE_NEWS = 302000;// 新闻类
+	
+	public static final int CODE_COOKBOOK = 308000;// 菜谱类
+	
+}
